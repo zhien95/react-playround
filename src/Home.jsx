293924ed4b5e4cccb9daa2e6530e59b854d484d2
zhien95 <33336170+zhien92@users.noreply.github.com>
2023-07-08 23:42:54 +0800

    add DraggableHoc.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,25 @@
-const Home = () => (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-)
+import DraggableHoc from "./DraggableHoc";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prev => prev + 1)
+    }, 100)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  return (
+    <div>
+      <DraggableHoc>
+        <h1>Home Page {count}</h1>
+      </DraggableHoc>
+    </div>
+  )
+}
 
 export default Home
